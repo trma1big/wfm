224eeb6336ca28b2e57bd08a75f9c1848bbab7fc
trma1big74 <trma1big@gmail.com>
2022-09-12 15:21:28 +0200

    clean installation

--- install.bat
+++
@@ -1,6 +0,0 @@
-
-npm init
-copy .env.ditrubution .env
-copy db/wfm.json.distribution db/wfm.json
-copy db/user.json.distribution db/user.json
-@REM node install.js
--- install.js
+++ install.js
@@ -2,32 +2,56 @@ require('dotenv').config()
 // var glob= require('./module'); 
 
 const crypto = require('crypto');
-const algorithm = 'aes-256-cbc'; 
+
 require('dotenv').config()
 // const key = Buffer.from(process.env.key_crypt, 'hex');
 // const iv = Buffer.from(process.env.iv_crypt, 'hex');
 
 
-
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+  charactersLength));
+   }
+   return result;
+  }
 
 //Encrypting text
-function encrypt(text) {
-   let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-   let encrypted = cipher.update(text);
-   encrypted = Buffer.concat([encrypted, cipher.final()]);
-   return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+function encrypt(text,key,iv) {
+    keyb = Buffer.from(key, 'hex'); 
+    ivb = Buffer.from(iv, 'hex'); 
+    algorithm = 'aes-256-cbc'; 
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(keyb), ivb);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
 }
 
-// Decrypting text
-function decrypt(text) {
+function create_password(password, iv, key) {  
+    const fs = require('fs'); 
+    let rawdata = fs.readFileSync('db/users.json');                    
+    let users = JSON.parse(rawdata);
+    let new_password = encrypt(password,key,iv);
+    users.splice(0,1);
+    item = {};
+    item = {'username': 'admin',
+            'password':new_password,
+            'entity': 'ALL' ,
+            'admin': 1
+    };    
+    users.push(item);
+    const data = JSON.stringify(users);
   
-  let iv = Buffer.from(text.iv, 'hex');
-  let encryptedText = Buffer.from(text.encryptedData, 'hex');
-  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-  let decrypted = decipher.update(encryptedText);
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-  return decrypted.toString();
-}
+    fs.writeFile('db/users.json', data, (err) => {
+        if (err) {
+            throw err;
+        }  
+    });
+  };
+
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -36,78 +60,55 @@ const rl = readline.createInterface({
 
 var fs = require('fs');
 
-rl.question('do you want to copy distribution files with default conf (Y/N DEFAULT Y)?', function (answer) {
-    if ( answer === "N") { rl.close();return;}
-    else {if ((answer !== "Y") && (answer !== "")) { console.log("you have to answer Y or N....."); rl.close(); return;}}
-    fs.copyFile('.env.distribution', '.env', (err) => {
-        if (err) throw err;
-        console.log('File .env was copied to destination');
-    });
-    fs.copyFile('db/wfm.json.distribution', 'db/wfm.json', (err) => {
-        if (err) throw err;
-        console.log('File db/wfm.json was copied to destination');
-    });
-    fs.copyFile('db/user.json.distribution', 'db/user.json', (err) => {
-        if (err) throw err;
-        console.log('File db/user.json was copied to destination');
-    });
-    console.log("START CONFIGURING ADMIN ACCESS AND ENV PARAMETER.........")
-    console.log("You will have to insert the Key Crypt used to crypt user password (tipically 65 chars)")
-    console.log("example: 2e8224a7ba5c5a67ca6e018bec55f3936aa11469667142e0deb4c2604e646d90 ")
 
-    console.log("You will have to insert the iv crypt used to crypt user password (tipically more than 30 chars)")
-    console.log("example: 95b243eac24577b79eeec50dd1427660 ")
-
-
-    rl.question('key crypt:', function (key_crypt) {
-        rl.question('iv crypt:', function (IVCrypt) {
-            fs.readFile(".env2.distribution", 'utf8', function (err,data) {
-            if (err) {
-                return console.log(err);
-            }
-            var result = data.replace(/key_crypt =/g, 'key_crypt = ' + key_crypt);
-            var result_final = result.replace(/iv_crypt =/g, 'iv_crypt = ' + IVCrypt);
-
-            fs.writeFile(".env2.distribution", result_final, 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
-            });
-            rl.question('Inset Admin user password: ', function (password) {
-                console.log(`${key_crypt}, is a citizen of ${IVCrypt} and password is  ${password}`);
-                rl.close();
-            });
+const question1 = () => {
+    return new Promise((resolve, reject) => {
+      rl.question('do you want to copy distribution files with default conf (Y/N DEFAULT Y)? ', (answer) => {         
+        resolve(answer)
+      })
+    })
+  }
+  
+  const question2 = () => {
+    return new Promise((resolve, reject) => {
+      rl.question('Insert Admin user password: ', (password) => {               
+        resolve(password)
+      })
+    })
+  }
+  
+  const main = async () => {
+    await question1().then(answer => {
+        if ( answer === "N") { rl.close();return;}
+        else {if ((answer !== "Y") && (answer !== "")) { console.log("you have to answer Y or N....."); rl.close(); return;}}
+        fs.copyFile('.env.distribution', '.env', (err) => {
+            if (err) throw err;            
         });
+        fs.copyFile('db/wfm.json.distribution', 'db/wfm.json', (err) => {
+            if (err) throw err;        
+        });
+        fs.copyFile('db/users.json.distribution', 'db/users.json', (err) => {
+            if (err) throw err;            
+        });    
     });
-});
-rl.on('close', function () {
-  console.log('\nExiting from installation script !!!');
-  process.exit(0);
-});
+   
+    const password = "";
+    const kcrypt = crypto.randomBytes(32);
+    const IVCrypt = crypto.randomBytes(16);
+    const text_kcrypt = kcrypt.toString('hex');
+    const text_IVCrypt = IVCrypt.toString('hex');   
+    await question2().then(password => {
+        const data = fs.readFileSync(".env",{encoding:'utf8', flag:'r'});        
+        var result = data.replace(/key_crypt=/g, 'key_crypt= ' + text_kcrypt);
+        var result_final = result.replace(/iv_crypt=/g, 'iv_crypt= ' + text_IVCrypt);        
+        fs.writeFile(".env", result_final, function (err) {
+            if (err) return console.log(err);
+        });             
+        create_password(password, IVCrypt, kcrypt);
+    });   
+    
+    rl.close()
+    console.log("Successfull created new crypting token and change the admin password please login with 'admin' and new password.")
+  }
 
-
-// const fs = require('fs'); 
-// let rawdata = fs.readFileSync(process.env.CONF_FOLDER + '/users.json');
-// let users = JSON.parse(rawdata);
-
-// if (typeof req.body.id !== 'undefined' && req.body.id !== '') {     
-//   users.splice(parseInt(req.body.id),1)
-// }
-// if (typeof req.body.admin == 'undefined') { req.body.admin = 0; }
-// if (req.body.entity == '') { req.body.entity= "ALL"; }
-// let password = glob.encrypt(req.body.password);
-
-// item = {};
-// item = {'username': req.body.username.toLowerCase(),
-//         'password':password,
-//         'entity':req.body.entity ,
-//         'admin': parseInt(req.body.admin)
-// };
-
-// users.push(item);
-// const data = JSON.stringify(users);
-
-// fs.writeFile(process.env.CONF_FOLDER + '/users.json', data, (err) => {
-//     if (err) {
-//         throw err;
-//     }
-// });
+main()
