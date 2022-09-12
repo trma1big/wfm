fc900fae006a1d1293a84ca33becdd56c365d2a4
trma1big74 <trma1big@gmail.com>
2022-09-12 11:55:46 +0200

    install

--- install.js
+++ install.js
@@ -1,7 +1,33 @@
 require('dotenv').config()
-var glob= require('./module'); 
+// var glob= require('./module'); 
+
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc'; 
+require('dotenv').config()
+// const key = Buffer.from(process.env.key_crypt, 'hex');
+// const iv = Buffer.from(process.env.iv_crypt, 'hex');
 
 
+
+
+//Encrypting text
+function encrypt(text) {
+   let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+   let encrypted = cipher.update(text);
+   encrypted = Buffer.concat([encrypted, cipher.final()]);
+   return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+// Decrypting text
+function decrypt(text) {
+  
+  let iv = Buffer.from(text.iv, 'hex');
+  let encryptedText = Buffer.from(text.encryptedData, 'hex');
+  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
